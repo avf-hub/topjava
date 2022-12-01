9c0957331dbb87dddb92191eb415c654c336781d
Andrey Falkovskiy <avf-91@yandex.ru>
2022-12-01 22:58:04 +0300

    implement HW_07 with optional

---
+++ src/main/java/ru/javawebinar/topjava/web/formaters/LocalDateFormatter.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.topjava.web.formaters;
+
+import org.springframework.format.Formatter;
+import ru.javawebinar.topjava.util.DateTimeUtil;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public final class LocalDateFormatter implements Formatter<LocalDate> {
+
+    @Override
+    public LocalDate parse(String text, Locale locale) throws ParseException {
+        return DateTimeUtil.parseLocalDate(text);
+    }
+
+    @Override
+    public String print(LocalDate ld, Locale locale) {
+        return ld.format(DateTimeFormatter.ISO_LOCAL_DATE);
+    }
+}
